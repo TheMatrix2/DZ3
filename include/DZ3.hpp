1841bc8e7eafebf70ac7ff7257a002c33a196321
thematrix <ermolovich.boris@yandex.ru>
2022-12-22 15:58:54 +0300

    написание алгоритма

--- include/DZ3.hpp
+++ include/DZ3.hpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+long int discriminant(int a, int b, int c);
+void add_to_file(const vector<char> m, const string name_file);
+
 #ifndef DZ3_DZ3_H
 #define DZ3_DZ3_H
 
--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,24 @@
 #include "DZ3.hpp"
 
 int main(int argc, char * argv []){
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int c = atoi(argv[3]);
+    int k = atoi(argv[4]);
+    int i;
+    vector<char> vec;
 
+    while(i < k) {
+        int d = discriminant(a, b, c) % INT32_MAX;
+        if (d % 2 == 0)
+            vec.push_back(0);
+        else
+            vec.push_back(1);
+        c = b;
+        b = a;
+        a = d % 128;
+        i++;
+    }
+
+    add_to_file(vec, "myfile.bin");
 }
